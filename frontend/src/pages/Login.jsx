5eb8fec98415aa73ee631ec920350adb1d1d405f
Hong Wei Tai <e1138406@u.nus.edu>
2025-04-04 00:39:39 +0800

    Redirect login to register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -48,22 +48,22 @@ const Login = () => {
                         /> 
                     </div>
                     <div className="mb-3 text-start">
-                        <label htmlFor="exampleInputPassword1" className="form-label">
+                        <label htmlFor="password" className="form-label">
                             <strong>Password</strong>
                         </label>
                         <input 
                             type="password" 
                             placeholder="Enter Password"
                             className="form-control" 
-                            id="exampleInputPassword1" 
+                            id="password" 
                             onChange={(event) => setPassword(event.target.value)}
                             required
                         />
                     </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
+                    <button type="submit">Login</button>
                 </form>
-                <p className='container my-2'>Don&apos;t have an account?</p>
-                {/* <Link to='/register'>Register</Link> */}
+                <p>Don&apos;t have an account?</p>
+                <Link to='/register'>Register</Link>
             </div>
         </LandingLayout>
     )
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
