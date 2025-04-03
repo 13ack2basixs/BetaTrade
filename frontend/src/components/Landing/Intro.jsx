a4f6271c3ff93c32d519a5ef1e93c66aed612ff3
Hong Wei Tai <e1138406@u.nus.edu>
2025-04-03 23:15:09 +0800

    Add landing page components

---
+++ frontend/src/components/Landing/Intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="intro-container">
+        <h1>Welcome to BetaTrade</h1>
+        <p>This app hopes to provide you with a safe space to explore investing, join us and have fun!</p>
+        <button>Join Us</button>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
