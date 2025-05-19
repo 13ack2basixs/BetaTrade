9e43923ec9235033072576d533b020c4f637f7cf
Hong Wei Tai <e1138406@u.nus.edu>
2025-05-19 15:13:00 +0800

    Fix allowed origins

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ['http://localhost:5173', 'https://betatrade-sxbm.onrender.com'];
+const allowedOrigins = ['http://localhost:5173', 'https://betatrade-sxbm.onrender.com', 'https://betatrade.vercel.app'];
 
 const corsOptions = {
     origin: (origin, callback) => {
